4621bda71d80e2b2f39083cb4cf0da41bae37520
Max <tutv95@gmail.com>
2017-07-31 15:01:56 +0700

    An amazing todo app.

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+        <header>
+            <span>Vue.js PWA</span>
+        </header>
+        <main>
+            <img src="./assets/logo.png" alt="Vue.js PWA">
+            <router-view></router-view>
+        </main>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  }
+</script>
